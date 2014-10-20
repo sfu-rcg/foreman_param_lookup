064215107da6ea07895bd46da5d0a132830d6409
Riley Shott <rshott@sfu.ca>
2014-10-20 16:59:53 -0700

    Modified clientcert lookup
    When a host is created through the Foreman web interface, it will set the
    record's certname to nil, and this can cause a lookup failure when using a
    Hiera backend (ex: Fancyass). So for the clientcert lookup only, we now check
    to see if there is a host with a matching name as the passed clientcert value.
    If there is, we check to see if its certname value is nil. If it is, we can
    safely assume that this is the record we intended to find.

--- app/controllers/foreman_param_lookup/classifications_controller.rb
+++ app/controllers/foreman_param_lookup/classifications_controller.rb
@@ -8,6 +8,14 @@ module ForemanParamLookup
         output host, params[:fqdn]
       when 'clientcert'
         host = Host.find_by_certname params[:clientcert]
+        if host.nil?
+          host = Host.find_by_name params[:clientcert]
+          # Be safe, return nil if we found a host with a maching name, but its
+          # certname value isn't nil
+          # The .certname method in Host::Managed is being overriden
+          # to a return a host's hostname if its certname is nil
+          host = nil unless host.send(:read_attribute, :certname) == nil
+        end
         output host, params[:clientcert]
       when 'macaddress'
         host = Host.find_by_mac params[:macaddress]
